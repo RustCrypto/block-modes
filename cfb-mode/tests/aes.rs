29ab1c22b2b12573e7dc5a6e8bdaa011e76f4716
Friedel Ziegelmayer <me@dignifiedquire.com>
2022-09-13 05:51:49 +0200

    cfb-mode: add Buffer{Encryptor|Decryptor} (#17)

--- cfb-mode/tests/aes.rs
+++ cfb-mode/tests/aes.rs
@@ -1,6 +1,6 @@
 use aes::*;
-use cfb_mode::{Decryptor, Encryptor};
-use cipher::{block_mode_dec_test, block_mode_enc_test, iv_state_test};
+use cfb_mode::{BufDecryptor, BufEncryptor, Decryptor, Encryptor};
+use cipher::{block_mode_dec_test, block_mode_enc_test, iv_state_test, KeyInit};
 
 iv_state_test!(aes128_cfb_enc_iv_state, Encryptor<Aes128>, encrypt);
 iv_state_test!(aes128_cfb_dec_iv_state, Decryptor<Aes128>, decrypt);
@@ -51,5 +51,67 @@ fn aes128_cfb_async_test() {
         let dec = Dec::new_from_slices(&key, &iv).unwrap();
         dec.decrypt(t);
         assert_eq!(t, &pt[..i]);
+    }
+}
+
+#[test]
+fn aes128_cfb_buffered_test() {
+    use cipher::{AsyncStreamCipher, KeyIvInit};
+
+    type Enc = Encryptor<Aes128>;
+
+    type BufEnc = BufEncryptor<Aes128>;
+    type BufDec = BufDecryptor<Aes128>;
+
+    let key = [42; 16];
+    let iv = [24; 16];
+    let mut pt = [0u8; 101];
+    for (i, b) in pt.iter_mut().enumerate() {
+        *b = (i % 11) as u8;
+    }
+
+    // unbuffered
+    let enc = Enc::new_from_slices(&key, &iv).unwrap();
+    let mut ct = pt.clone();
+    enc.encrypt(&mut ct);
+
+    // buffered
+    for i in 1..100 {
+        let mut buf_enc = BufEnc::new_from_slices(&key, &iv).unwrap();
+        let mut ct2 = pt.clone();
+        for chunk in ct2.chunks_mut(i) {
+            buf_enc.encrypt(chunk);
+        }
+        assert_eq!(ct2, ct);
+
+        let mut buf_dec = BufDec::new_from_slices(&key, &iv).unwrap();
+        for chunk in ct2.chunks_mut(i) {
+            buf_dec.decrypt(chunk);
+        }
+        assert_eq!(ct2, pt);
+    }
+
+    // buffered with restore
+    for i in 1..100 {
+        let mut buf_enc = BufEnc::new_from_slices(&key, &iv).unwrap();
+        let mut ct2 = pt.clone();
+        for chunk in ct2.chunks_mut(i) {
+            let (iv, pos) = buf_enc.get_state();
+            let cipher = Aes128::new_from_slice(&key).unwrap();
+            buf_enc = BufEnc::from_state(cipher, iv, pos);
+
+            buf_enc.encrypt(chunk);
+        }
+        assert_eq!(ct2, ct);
+
+        let mut buf_dec = BufDec::new_from_slices(&key, &iv).unwrap();
+        for chunk in ct2.chunks_mut(i) {
+            let (iv, pos) = buf_dec.get_state();
+            let cipher = Aes128::new_from_slice(&key).unwrap();
+            buf_dec = BufDec::from_state(cipher, iv, pos);
+
+            buf_dec.decrypt(chunk);
+        }
+        assert_eq!(ct2, pt);
     }
 }
