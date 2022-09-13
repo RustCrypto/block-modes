93ddeb15847c920a37f46b5039ddc04713d75002
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2022-09-13 07:03:26 +0300

    Remove html_root_url

--- ctr/src/lib.rs
+++ ctr/src/lib.rs
@@ -58,9 +58,8 @@
 
 #![no_std]
 #![doc(
-    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg",
-    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg",
-    html_root_url = "https://docs.rs/ctr/0.9.1"
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg"
 )]
 #![forbid(unsafe_code)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
