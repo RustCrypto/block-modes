a6588eb78ec800e5e7074625e861d013dc39770c
Alexandr Kitaev <makavity231@gmail.com>
2023-03-30 18:39:08 +0300

    fix/revert no_std

--- belt-ctr/src/lib.rs
+++ belt-ctr/src/lib.rs
@@ -37,7 +37,7 @@
 //!
 //! [1]: https://apmi.bsu.by/assets/files/std/belt-spec371.pdf
 
-// #![no_std]
+#![no_std]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg"
