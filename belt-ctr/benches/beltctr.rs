9a47d60a4b82e0b5ef35a75e0699ff63033b8cd1
Alexandr Kitaev <makavity231@gmail.com>
2023-03-30 18:49:28 +0300

    fix/bench

--- belt-ctr/benches/beltctr.rs
+++ belt-ctr/benches/beltctr.rs
@@ -1,11 +1,8 @@
 #![feature(test)]
 extern crate test;
 
-use belt_block::BeltBlock;
-use belt_ctr::flavor::ctr128::Ctr128LE;
-
 cipher::stream_cipher_bench!(
-    belt_ctr::BeltCtr<BeltBlock, Ctr128LE>;
+    belt_ctr::BeltCtr;
     ctr_128le_belt128_stream_bench1_16b 16;
     ctr_128le_belt128_stream_bench2_256b 256;
     ctr_128le_belt128_stream_bench3_1kib 1024;
