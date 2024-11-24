b7126d9513ba9c7ecfc5923b73cce9f4a48d8605
zer0x64 <dugre.philippe@hotmail.com>
2024-11-24 14:10:22 -0500

    [XTS] Fix benchmarks

--- xts/benches/aes.rs
+++ xts/benches/aes.rs
@@ -4,25 +4,25 @@ extern crate test;
 use aes::{ Aes128, Aes256 };
 
 cipher::block_encryptor_bench!(
-    KeyIv: xts::SplitEncryptor<Aes128>,
+    KeyIv: xts::Encryptor<Aes128>,
     xts_aes128_encrypt_block,
     xts_aes128_encrypt_blocks,
 );
 
 cipher::block_decryptor_bench!(
-    KeyIv: xts::SplitDecryptor<Aes128, Aes128>,
+    KeyIv: xts::Decryptor<Aes128>,
     xts_aes128_decrypt_block,
     xts_aes128_decrypt_blocks,
 );
 
 cipher::block_encryptor_bench!(
-    KeyIv: xts::SplitEncryptor<Aes256>,
+    KeyIv: xts::Encryptor<Aes256>,
     xts_aes256_encrypt_block,
     xts_aes256_encrypt_blocks,
 );
 
 cipher::block_decryptor_bench!(
-    KeyIv: xts::SplitDecryptor<Aes256, Aes256>,
+    KeyIv: xts::Decryptor<Aes256>,
     xts_aes256_decrypt_block,
     xts_aes256_decrypt_blocks,
 );
